5733601f9407cfdae11d1357c2c399d51199fdcf
Maciej Suchecki <mc.suchecki@gmail.com>
2012-05-18 19:48:35 +0200

    Added - Battleships main class

---
+++ Battleships.java
@@ -0,0 +1,14 @@
+package pl.mc.battleships;
+
+import pl.mc.battleships.view.*;
+
+/**
+ * @author Maciej 'mc' Suchecki
+ * Battleships game main class. 
+ */
+public class Battleships {
+  public static void main(String[] args) {
+    View view = View.getInstance();
+    view.showFrame();
+  }
+}
